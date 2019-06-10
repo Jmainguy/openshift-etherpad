9d7758f5a569bbff32c054148f47a3fd8b808ad5
Jonathan Mainguy <jon@soh.re>
2019-06-10 11:37:44 -0400

    update from node3 to node 12

--- setup.sh
+++ setup.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-yum install -y epel-release
-# Because EPEL broke nodejs package
-# See https://bugs.centos.org/view.php?id=13669&nbn=1
-yum install -y https://kojipkgs.fedoraproject.org//packages/http-parser/2.7.1/3.el7/x86_64/http-parser-2.7.1-3.el7.x86_64.rpm
+# https://github.com/nodesource/distributions
+curl -sL https://rpm.nodesource.com/setup_12.x | bash -
 yum install -y make gcc-c++ nodejs git
-cd /opt
-git clone https://github.com/ether/etherpad-lite
+git clone https://github.com/ether/etherpad-lite /opt/etherpad-lite
 chmod -R 777 /opt/etherpad-lite
 yum clean all
